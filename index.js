322a9e034260d4c5e0b4a70ef4d86f1434bcfa26
zefetorres <zefetorres@icloud.com>
2019-09-22 20:25:14 -0500

    add schema, resolvers and config web

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync }  = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+// definiendo el esquema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
+
+// Ruta
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listen at http://localhost:${port}/api`)
+})
